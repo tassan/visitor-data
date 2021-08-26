31301bf745d083413529b71dc5f555f05d02ff25
Flávio Tassan <ftassan@outlook.com>
2021-08-26 17:15:19 -0300

    First page template

--- app.py
+++ app.py
@@ -4,15 +4,24 @@ from flask.app import Flask
 import socket as s
 import os
 from dotenv import load_dotenv
+from flask.templating import render_template
 import requests
 from markupsafe import escape
 
 load_dotenv(".env")
 
 app = Flask(__name__)
-
-# DATABASE = os.getenv("DATABASE")
 KEY = os.getenv("KEY")
+
+
+@app.route("/")
+def index():
+    hostname = s.gethostname()
+    ip = s.gethostbyname(hostname)
+    url = f"https://ipgeolocation.abstractapi.com/v1/?api_key={KEY}&ip_address={ip}"
+    response = requests.get(url)
+    geo = response.json()
+    return render_template('index.html', content=geo)
 
 
 @app.route("/ip", methods=["GET"])
@@ -27,12 +36,12 @@ def get_geo():
     ip = s.gethostbyname(hostname)
     url = f"https://ipgeolocation.abstractapi.com/v1/?api_key={KEY}&ip_address={ip}"
     response = requests.get(url)
-    return jsonify(response.content)
+    return response.content
 
 @app.route("/geo/<ip>", methods=["GET"])
 def get_geo_by_ip(ip):
     ip_address = escape(ip)
     url = f"https://ipgeolocation.abstractapi.com/v1/?api_key={KEY}&ip_address={ip_address}"
     response = requests.get(url)
-    return jsonify(response.content)
+    return response.content
 
